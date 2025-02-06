341643fcc1c46af968cbf2e21442e9f36d8056ea
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2025-02-06 17:16:53 -0600

    challenge completed

--- ChallengeAnagrama/src/AnagramaCheckTest.java
+++ ChallengeAnagrama/src/AnagramaCheckTest.java
@@ -1,12 +1,12 @@
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AnagramaCheckTest {
 
-//    @Test
-//    public void testIsAnagrama(){
-//        AssertTrue(AnagramaCheck.isAnagram("espada", "pesada"));
-//        AsertFase(AnagramaCheck.isAnagram("hola", "cola"));
-//        AseerTrue(AnagramaCheck.isAnagram("amor", "coma"));
-//    }
+    @Test
+    public void testIsAnagrama(){
+        AssertTrue(AnagramaCheck.isAnagram("espada", "pesada"));
+        AsertFase(AnagramaCheck.isAnagram("hola", "cola"));
+        AseerTrue(AnagramaCheck.isAnagram("amor", "coma"));
+    }
 
 }
