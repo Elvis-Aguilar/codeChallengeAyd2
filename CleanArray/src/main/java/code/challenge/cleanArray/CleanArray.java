bb52e94f46700f87a8a36211d14875a3addc012d
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2025-02-13 17:46:25 -0600

    challenge completed clean array

---
+++ CleanArray/src/main/java/code/challenge/cleanArray/CleanArray.java
@@ -0,0 +1,79 @@
+package code.challenge.cleanArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CleanArray {
+
+    /*
+     *  solucion sin arrayList
+     */
+
+    public static Object[] cleanArray(Object[] array) {
+
+        if (array == null || array.length == 0) {
+            return new Object[0];
+        }
+
+        int size = CleanArray.sizeArray(array);
+        Object[] arrayResult = new Object[size];
+
+        int index = 0;
+
+        CleanArray.concatArrays(arrayResult, array, index);
+
+        return arrayResult;
+    }
+
+    public static void concatArrays(Object[] array1, Object[] array2, int index) {
+        for (int i = index; i < array1.length; i++) {
+            if (array1[i] instanceof Object[]) {
+                Object[] temp = (Object[]) array1[i];
+                CleanArray.concatArrays(array1, temp, i);
+            }else{
+                array2[i] = array1[i];
+            }
+        }
+        //return index;
+    }
+
+    public static int sizeArray(Object[] array) {
+        int cont = 0;
+        if (array == null || array.length == 0) {
+            return cont;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] instanceof Object[]) {
+                Object[] temp = (Object[]) array[i];
+                cont += CleanArray.sizeArray(temp);
+            }else {
+                cont++;
+            }
+        }
+
+        return cont;
+    }
+
+//    public static Object[] cleanArray(Object[] array) {
+//        if (array == null || array.length == 0) {
+//            return new Object[0];
+//        }
+//
+//        List<Object> resultList = new ArrayList<>();
+//        flattenArray(array, resultList);
+//
+//        return resultList.toArray();
+//    }
+//
+//    private static void flattenArray(Object[] array, List<Object> resultList) {
+//        for (Object element : array) {
+//            if (element instanceof Object[]) {
+//                // Llamada recursiva para aplanar subarrays
+//                flattenArray((Object[]) element, resultList);
+//            } else {
+//                resultList.add(element);
+//            }
+//        }
+//    }
+
+}
---
+++ CleanArray/src/test/java/code/challenge/cleanArray/CleanArrayTest.java
@@ -0,0 +1,24 @@
+package code.challenge.cleanArray;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CleanArrayTest {
+
+    @Test
+    void testCleanArrayCorrectly() {
+        assertArrayEquals(new Object[]{1, 2, "3", "4"}, CleanArray.cleanArray(new Object[]{1, 2, new Object[]{"3", "4"}}));
+        assertArrayEquals(new Object[]{1, 2, 4, 3, 5, 6}, CleanArray.cleanArray(new Object[]{1, 2, new Object[]{3,4, new Object[]{5,6}}}));
+        assertArrayEquals(new Object[]{1, 2, "a", "b", 5, 6},  CleanArray.cleanArray(new Object[]{1, 2, new Object[]{"a", "b", new Object[]{5,6}}}));
+        assertArrayEquals(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, CleanArray.cleanArray(new Object[]{1, 2, new Object[]{3,4, new Object[]{5,6,new Object[]{7,8, new Object[]{9,10}}}}}));
+    }
+
+    @Test
+    void testCleanArrayError() {
+        assertArrayEquals(new Object[]{1, 2, "3", "4"}, CleanArray.cleanArray(new Object[]{1, 2, new Object[]{"3", "4"}}));
+        assertArrayEquals(new Object[]{1, 2, 4, 3, 5, 6}, CleanArray.cleanArray(new Object[]{1, 2, new Object[]{3,4, new Object[]{5,6}}}));
+        assertArrayEquals(new Object[]{1, 2, "a", "b", 5, 6},  CleanArray.cleanArray(new Object[]{1, 2, new Object[]{"a", "b", new Object[]{5,6}}}));
+        assertArrayEquals(new Object[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, CleanArray.cleanArray(new Object[]{1, 2, new Object[]{3,4, new Object[]{5,6,new Object[]{7,8, new Object[]{9,10}}}}}));
+    }
+}
